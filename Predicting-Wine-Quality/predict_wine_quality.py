0bc7ff6e9ee9c6cccff6a2658e703c4668983d5a
ayberkozcan <151998101+ayberkozcan@users.noreply.github.com>
2024-06-17 18:06:01 +0300

    Update predict_wine_quality.py
    The dataset was modeled using a PLS regression model

--- Predicting-Wine-Quality/predict_wine_quality.py
+++ Predicting-Wine-Quality/predict_wine_quality.py
@@ -1,6 +1,6 @@
 # Predicting Wine Quality
 
-# Comparison of PCR and PLS Regression Models
+# Comparison of PLS and PCR Regression Models
 
 # Dataset: Wine Quality Dataset
 
@@ -10,8 +10,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+
 
 wine = pd.read_csv("WineQT.csv")
 
@@ -80,4 +79,38 @@ corr_quality = corr_matrix["quality"].sort_values(ascending=False)
 print("Correlations with quality")
 print(corr_quality)
 
-# We are going to use alcohol, sulphates, citric acid and volatile acidity as undependent variables
+
+# Modeling 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+y = df["quality"]
+X = df.drop(columns=["quality"])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# PLS Regression Model
+from sklearn.cross_decomposition import PLSRegression
+
+pls_model = PLSRegression()
+pls_model.fit(X_train, y_train)
+
+y_pred_pls = pls_model.predict(X_test)
+
+pls_rmse = np.sqrt(mean_squared_error(y_test, y_pred_pls))
+pls_r2 = r2_score(y_test, y_pred_pls)
+
+# Modeling with reduced components
+pls_model_reduced = PLSRegression(n_components=3)  
+pls_model_reduced.fit(X_train, y_train)
+
+y_pred_pls_reduced = pls_model_reduced.predict(X_test)
+
+pls_rmse_reduced = np.sqrt(mean_squared_error(y_test, y_pred_pls_reduced))
+pls_r2_reduced = r2_score(y_test, y_pred_pls_reduced)
+
+print("\nPLS RMSE:", pls_rmse)
+print("PLS R2 score:", pls_r2)
+
+print("\nPLS RMSE (reduced_component):", pls_rmse_reduced)
+print("PLS R2 score (reduced_component):", pls_r2_reduced)
